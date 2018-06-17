92327a951514d20a7a18523c9aafa3bd19d76d3a
Bob Shelton <inhortte@gmail.com>
2018-06-17 18:05:42 +0200

    thurk

--- lib/app/router.ex
+++ lib/app/router.ex
@@ -24,6 +24,7 @@ defmodule App.Router do
 
   def generate_message_matcher(handler) do
     quote do
+      IO.inspect var!(update)
       def do_match_message(var!(update)) do
         handle_message unquote(handler), [var!(update)]
       end
